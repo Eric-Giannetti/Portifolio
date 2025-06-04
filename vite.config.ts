fc365b14de7d0a1ff0fda95f04e7ccfb5f7917e2
Eric-Giannetti <ericbgiant@gmail.com>
2025-06-04 16:42:28 -0300

    alteração nome repositório

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/eric-giannetti/',
+  base: '/Portifolio',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
